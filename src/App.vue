8090616998dd4079164f709fc84f6999244b17dd
youjin <yjinhann@gmail.com>
2023-04-05 16:56:00 +0900

    add: todo done 속성추가

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,23 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 
 // 각 todo에 고유한 id 부여
 let id = 0
 
 const newTodo = ref('')
+const hideCompleted = ref(false)
 const todos = ref([
-  { id: id++, text: 'HTML 배우기' },
-  { id: id++, text: 'JavaScript 배우기' },
-  { id: id++, text: 'Vue 배우기' }
+  { id: id++, text: 'HTML 배우기', done: true },
+  { id: id++, text: 'JavaScript 배우기', done: true },
+  { id: id++, text: 'Vue 배우기', done: false }
 ])
 
+const filteredTodos = computed(() => {
+  return hideCompleted.value ? todos.value.filter((t) => !t.done) : todos.value
+})
+
 function addTodo() {
-  todos.value.push({ id: id++, text: newTodo.value })
+  todos.value.push({ id: id++, text: newTodo.value, done: false })
   newTodo.value = ''
 }
 
@@ -24,12 +29,22 @@ function removeTodo(todo) {
 <template>
   <form @submit.prevent="addTodo">
     <input v-model="newTodo" :key="todos.id" />
-    <button>할 일 추가</button>
+    <button>Add Todo</button>
   </form>
   <ul>
-    <li v-for="todo in todos" :key="todo.in">
-      {{ todo.text }}
+    <li v-for="todo in filteredTodos" :key="todo.id">
+      <input type="checkbox" v-model="todo.done" />
+      <span :class="{ done: todo.done }">{{ todo.text }}</span>
       <button @click="removeTodo(todo)">X</button>
     </li>
   </ul>
+  <button @click="hideCompleted = !hideCompleted">
+    {{ hideCompleted ? 'Show all' : 'Hide completed' }}
+  </button>
 </template>
+
+<style>
+.done {
+  text-decoration: line-through;
+}
+</style>
